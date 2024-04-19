22364bb4971f2072559581b3f2e522bbfd2f8937
Jonas Heineson <heineson@gmail.com>
2024-04-19 14:17:33 +0200

    Add Lazygit install script

--- install.bash
+++ install.bash
@@ -27,6 +27,7 @@ else
     ln -s $SS_SOURCE $SS_DEST
 fi
 
-echo "Install configures plugins in vim with ':PlugInstall'"
+echo "- Install configures plugins in vim with ':PlugInstall'"
+echo "- Check ./scripts and run the useful ones"
 echo "Done"
 
---
+++ scripts/lazygit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+cd /tmp
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
