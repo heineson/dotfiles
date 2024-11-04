100cb05c30bd09d3219914ef994d6d2393fc0baf
Jonas Heineson <heineson@gmail.com>
2024-11-04 07:56:11 +0100

    Clone config repo with nvim install

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -12,3 +12,16 @@ sudo mv nvim.appimage /opt/nvim/nvim
 mkdir -p ~/bin
 ln -s /opt/nvim/nvim ~/bin/nvim
 
+repo_dest="${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+
+if [ ! -d "~/dev/$repo" ]; then
+  git clone git@github.com:heineson/kickstart.nvim.git
+  git clone git@github.com:heineson/kickstart.nvim.git "$repo_dest" 
+else
+  echo "Updating nvim config repo"
+  cd $repo_dest
+  git pull
+fi
+
+
+
